e5244dc3b7cc0a1b29b2ba6ad2951b884facb31b
Nikolay0710 <ooo19920710@mail.ru>
2019-06-27 23:05:25 +0300

    Update main.php

--- views/viewGuestBook/main.php
+++ views/viewGuestBook/main.php
@@ -1,10 +1,7 @@
-<div id="content">
-	<div id="post">
+
 		<img src="<?=TEMPLATE?>images/images.png" alt="" />
 		<h1>Fusce Eleifend Posuere Nibsor</h1>
 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eleifend posuere nibh, 
 		ut tincidunt est pretium vitae. Suspendisse dapibus arcu in tortor scelerisque volutpat. 
 		In imperdiet, lectus eu gravida vestibulum, orci bamte nibehre ustiyep malesuada nunc, 
 		lobortis commodo lorem ipsum a quam...<a href="#">Continue Reading</a></P>
-	</div>
-</div>
