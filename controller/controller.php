071fdea0abb59ea170e0411a8a04c5303f786a1e
Nikolay0710 <ooo19920710@mail.ru>
2019-07-01 22:19:26 +0300

    Update controller.php

--- controller/controller.php
+++ controller/controller.php
@@ -3,7 +3,7 @@
     /** ===> Контроллер приветствует вас! <=== **/
 
     //ini_set('display_errors', -1);
-	//error_reporting(E_ALL);
+    //error_reporting(E_ALL);
 
     // проверка на доступность константы \\
     defined('INDEX') or die('Access denied');
