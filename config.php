cf0d977a0e97098e3295c2a042bd0a0893edfe89
Nikolay0710 <ooo19920710@mail.ru>
2019-07-04 20:20:12 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
- 	/** ===> Конфигурационный файл приветствует вас! <=== **/
- 	defined('INDEX') or die('Access denied'); // Проверка на доступность константы \\
+ /** ===> Конфигурационный файл приветствует вас! <=== **/
+    defined('INDEX') or die('Access denied'); // Проверка на доступность константы \\
     
     define('PAST', 'http://guestBook.loc/');  // Имя домена \\
     define('MODEL', 'model/model.php'); // Имя модели //
@@ -10,17 +10,16 @@
     define('TEMPLATE', VIEW . 'viewGuestBook/'); // Активный шаблон \\
     define('COUNT_COMMENTS', 3); // Количество выводов комментарии в гостевой книге
 	
-	const DB_HOST = 'localhost';  // Сервер базы данных //
-	const DB_USER = 'root'; // Имя пользователя \\
-	const DB_PASS = ''; // Пароль пользователя //
-	const DB_NAME = 'guest_book'; // Имя базы данных \\
+    const DB_HOST = 'localhost';  // Сервер базы данных //
+    const DB_USER = 'root'; // Имя пользователя \\
+    const DB_PASS = ''; // Пароль пользователя //
+    const DB_NAME = 'guest_book'; // Имя базы данных \\
 
-	$db = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    $db = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
-	if (!$db) {
-	    echo "Ошибка: Невозможно установить соединение с MySQL." . PHP_EOL;
-	    echo "Код ошибки errno: " . mysqli_connect_errno() . PHP_EOL;
-	    echo "Текст ошибки error: " . mysqli_connect_error() . PHP_EOL;
-	    exit();
-	}
-	/** ===> Конфигурационный файл приветствует вас! <=== **/
+    if (!$db) {
+	 echo "Ошибка: Невозможно установить соединение с MySQL." . PHP_EOL;
+	 echo "Код ошибки errno: " . mysqli_connect_errno() . PHP_EOL;
+	 echo "Текст ошибки error: " . mysqli_connect_error() . PHP_EOL;
+	 exit();
+    }
