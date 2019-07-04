ccfe0652dfc51414277fa4feacee73fa6813d2d6
Nikolay0710 <ooo19920710@mail.ru>
2019-07-04 17:16:17 +0300

    Add files via upload

---
+++ functions/functions.php
@@ -0,0 +1,67 @@
+<?php
+
+	function print_arr($arr)
+	{
+		echo "<pre>";
+		print_r($arr);
+		echo "</pre>";
+	}
+
+	function redirect()
+	{
+		header("Location: ". $_SERVER["HTTP_REFERER"]);
+		exit;
+	}
+
+	function cleanHtmlCodes($cleanHtml)
+	{
+		$resCleanHtml = trim( strip_tags($cleanHtml) );
+		return $resCleanHtml;
+	}
+
+	/** ===== Постраничная навигация ===== */
+    function paginatio($page, $pages_count) {
+        if($_SERVER['QUERY_STRING']) { // если есть параметры в запросе
+            foreach($_GET as $key => $value) {
+                // формеруем строку пораметра без номера страницы ... номер передается параметром функции
+                if($key != 'page') $uri .= "$key=$value&amp;";
+            }
+        }
+        // формерование ссылок
+        $back = ''; // ссылка НАЗАД
+        $forward = ''; // ссыдка ВПЕРЕД
+        $startpage = ''; // ссылка в НАЧАЛО
+        $endpage = ''; // ссылка в КОНЕЦ
+        $page2left = ''; // пторая страница с лева
+        $page1left = ''; // первая страница с лева
+        $page2raight = ''; // пторая страница с справа
+        $page1raight = ''; // первая страница с справа
+        
+        if($page > 1) {
+            $back = "<a href='?{$uri}page=" . ($page - 1) . "' class='nav_link'>&lt;</a>";
+        }
+        if($page < $pages_count) {
+            $forward = "<a href='?{$uri}page=" . ($page + 1) . "' class='nav_link'>&gt;</a>";
+        }
+        if($page > 3) {
+            $startpage = "<a href='?{$uri}page=1' class='nav_link'>&laquo;</a>";
+        }
+        if($page < ($pages_count - 2)) {
+            $endpage = "<a href='?{$uri}page=" . $pages_count . "' class='nav_link'>&raquo;</a>";
+        }
+        if($page - 2 > 0) {
+            $page2left = "<a href='?{$uri}page=" . ($page - 2) . "' class='nav_link'>" . ($page - 2) . "</a>";
+        }
+        if($page - 1 > 0) {
+            $page1left = "<a href='?{$uri}page=" . ($page - 1) . "' class='nav_link'>" . ($page - 1) . "</a>";
+        }
+        if($page + 2 <= $pages_count) {
+            $page2raight = "<a href='?{$uri}page=" . ($page + 2) . "' class='nav_link'>" . ($page + 2) . "</a>";
+        }
+        if($page + 1 <= $pages_count) {
+            $page1raight = "<a href='?{$uri}page=" . ($page + 1) . "' class='nav_link'>" . ($page + 1) . "</a>";
+        }
+        // формируем вывод навигации
+        echo "<div class='paginatio'>" . $startpage . $back .  $page2left . $page1left . "<a class='nav_active'>" . $page . "</a>" . $page1raight . $page2raight . $forward . $endpage . "</div>";
+    }
+    /** ===== Постраничная навигация ===== */
