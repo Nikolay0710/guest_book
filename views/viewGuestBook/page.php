40610b4f4451786f0855f56c584f73217d516e9d
Nikolay0710 <ooo19920710@mail.ru>
2019-07-04 11:33:06 +0300

    Add files via upload

---
+++ views/viewGuestBook/page.php
@@ -0,0 +1,9 @@
+<?php if(!empty($page)): ?>
+	<h3><?=$page['titleSite']?></h3>
+	<?php if(isset($page['imgPage']) && $page['imgPage'] != 'no img'): ?>
+	<img src='<?=TEMPLATE?>images/<?=$page['imgPage']?>' alt='<?=$page['titleSite']?>' />
+	<?php endif; ?>
+	<?=$page['textSite']?>
+<?php else: ?>
+	<h3>Такой страницы нет на нашем сайте..</h3>
+<?php endif; ?>
